087e660231274af85e9e2016aaaa33c3f39f2d93
Damien Fleminks <damien@fleminks.com>
2020-11-01 19:46:36 +0100

    Fixed scanner code

--- app/src/main/java/com/example/salesforcescanner/MainActivity.kt
+++ app/src/main/java/com/example/salesforcescanner/MainActivity.kt
@@ -6,17 +6,21 @@ import android.os.Bundle
 import android.content.Intent;
 import android.widget.Toast;
 import android.widget.Button;
+import android.widget.TextView
 
 import com.google.zxing.integration.android.IntentIntegrator;
 
 class MainActivity : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val scanCodeBtn = findViewById<Button>(R.id.button_scan_code)
         scanCodeBtn.setOnClickListener {
-            //IntentIntegrator(this).initiateScan()
-            Toast.makeText(this, SalesforceApi.testCall(), Toast.LENGTH_LONG).show()
+            IntentIntegrator(this).initiateScan()
+            //Toast.makeText(this, SalesforceApi.testCall(), Toast.LENGTH_LONG).show()
         }
     }
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -27,6 +31,8 @@ class MainActivity : AppCompatActivity() {
                 Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show()
             } else {
                 Toast.makeText(this, "Scanned: " + result.contents, Toast.LENGTH_LONG).show()
+                val scanOutput: TextView = findViewById(R.id.scanOutput)
+                scanOutput.text = result.contents
             }
         }
     }
