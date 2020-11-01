0d5c9254305438721ba181b5b4af8c08c6e9106c
Damien Fleminks <damien@fleminks.com>
2020-11-01 20:45:42 +0100

    Add new activity

---
+++ app/src/main/java/com/example/salesforcescanner/ScanDetail.kt
@@ -0,0 +1,11 @@
+package com.example.salesforcescanner
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ScanDetail : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_scan_detail)
+    }
+}
