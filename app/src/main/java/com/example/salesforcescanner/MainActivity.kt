0324ed57c8b82f5e93fc8deade577862105cc85a
Damien Fleminks <damien@fleminks.com>
2021-01-02 16:52:32 +0100

    Added Rest API request

--- app/src/main/java/com/example/salesforcescanner/MainActivity.kt
+++ app/src/main/java/com/example/salesforcescanner/MainActivity.kt
@@ -1,13 +1,23 @@
 package com.example.salesforcescanner
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import android.content.Intent;
-import android.widget.Toast;
 import android.widget.ImageButton
-import com.google.zxing.integration.android.IntentIntegrator;
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.RequestQueue
+import com.google.zxing.integration.android.IntentIntegrator
 import java.text.SimpleDateFormat
 import java.util.*
+
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+
+import kotlinx.android.synthetic.main.activity_main.*
+import org.json.JSONObject
+
 
 const val SCAN_RESULT = "com.scanAnything.SCAN_RESULT"
 const val SCAN_DATE = "com.scanAnything.SCAN_DATE"
@@ -19,11 +29,22 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
         val scanCodeBtn = findViewById<ImageButton>(R.id.tapToScan)
         scanCodeBtn.setOnClickListener {
-            IntentIntegrator(this).initiateScan()
+            //IntentIntegrator(this).initiateScan()
+
         }
     }
+
+
+
+
+
+
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
@@ -31,16 +52,35 @@ class MainActivity : AppCompatActivity() {
             if (result.contents == null) {
                 Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show()
             } else {
-                Toast.makeText(this, "Scanned: " + result.contents, Toast.LENGTH_LONG).show()
-                var date = Date()
-                val formatter = SimpleDateFormat("MMM dd yyyy HH:mma")
-                val scanDate: String = formatter.format(date)
 
-                val intent = Intent(this, ScanDetail::class.java).apply {
-                    putExtra(SCAN_RESULT, result.contents)
-                    putExtra(SCAN_DATE, scanDate)
-                }
-                startActivity( intent)
+
+
+                //Volley for API request
+                val queue = Volley.newRequestQueue(this)
+                val url = "https://salesforce-qr-contacts01.herokuapp.com/contact" + result.contents
+                // Request a string response from the provided URL.
+                val stringRequest = StringRequest(
+                    Request.Method.GET, url,
+                    Response.Listener<String> { response ->
+                        // Display the first 500 characters of the response string.
+                        val obj = response
+                        //textView.text = response
+
+                        Toast.makeText(this, "Scanned: " + result.contents, Toast.LENGTH_LONG).show()
+                        var date = Date()
+                        val formatter = SimpleDateFormat("MMM dd yyyy HH:mma")
+                        val scanDate: String = formatter.format(date)
+
+                        val intent = Intent(this, ScanDetail::class.java).apply {
+                            putExtra(SCAN_RESULT, response)
+                            putExtra(SCAN_DATE, scanDate)
+                        }
+                        startActivity( intent)
+                    },
+                    Response.ErrorListener { textView.text = "That didn't work!" })
+
+                // Add the request to the RequestQueue.
+                queue.add(stringRequest)
 
             }
         }
--- app/src/main/java/com/example/salesforcescanner/SalesforceApi.java
+++ app/src/main/java/com/example/salesforcescanner/SalesforceApi.java
@@ -1,8 +1,18 @@
 package com.example.salesforcescanner;
 
-public class SalesforceApi {
+import java.io.*;
+import org.json.*;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
-    public static String testCall(){
-        return "HelloWorld";
-    }
+
+
+public class SalesforceApi extends Object{
+
+
+
 }
